8b39dc378f59f1050fbe7cf5acfea89ac76b4e1d
Zach Benton <Zach@Zach.local>
2016-10-31 08:31:46 -0500

    init

---
+++ src/main/java/com/theironyard/MyBingeTvController.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+/**
+ * Created by Zach on 10/31/16.
+ */
+public class MyBingeTvController {
+
+    
+    //TODO: Make a bad ass final project that will tottaly land us all an amazing job that we love and makes great money!!
+    //TODO: Have fun.
+}
---
+++ src/main/java/com/theironyard/User.java
@@ -0,0 +1,7 @@
+package com.theironyard;
+
+/**
+ * Created by Zach on 10/31/16.
+ */
+public class User {
+}
