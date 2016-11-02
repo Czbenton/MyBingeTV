2d15f68185c573d3ca458741f880f60f5f25470f
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2016-11-02 15:10:43 -0500

    working saved shows list

---
+++ src/main/java/com/theironyard/services/SavedShowRepo.java
@@ -0,0 +1,15 @@
+package com.theironyard.services;
+
+import com.theironyard.entities.SavedShow;
+import com.theironyard.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by jeremypitt on 11/2/16.
+ */
+public interface SavedShowRepo extends CrudRepository<SavedShow, Integer> {
+    SavedShow findFirstByShowid(String showid);
+    List<SavedShow> findAllByUser(User user);
+}
