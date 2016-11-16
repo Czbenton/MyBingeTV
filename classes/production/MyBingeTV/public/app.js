ed7ec1cb21d6386ee2b1a4bda977ffe6531c1fdf
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2016-11-16 13:03:09 -0600

    Ryan fixed it...

--- classes/production/MyBingeTV/public/app.js
+++ classes/production/MyBingeTV/public/app.js
@@ -18,26 +18,33 @@
     if (searchForm) {
         searchForm.addEventListener('submit', function (ev) {
             if (!searchFormInput || searchFormInput.value === '') {
-                alert('You must fill in the form');
                 ev.preventDefault();
-            }else {
-                var x = document.querySelector('.fa-spinner')
-                x.style.visibility="visible";
+                const hider = document.querySelector("#error-alert");
+
+                hider.classList.remove("cancel");
+                hider.classList.add("show-me");
+                const icon = document.querySelector(".icon");
+                icon.addEventListener('click', function (ev) {
+                    ev.preventDefault()
+                    const swoosh = document.querySelector("#error-alert");
+                    swoosh.classList.add("cancel");
+                });
+            } else {
+                var x = document.querySelector('.fa')
+                x.style.visibility = "visible";
             }
         });
     }
-
-    var searchForm2 = document.querySelector('.search-formSmall');
-    var searchFormInput2 = document.querySelector('.searchInputSmall');
-    if (searchForm2) {
-        searchForm2.addEventListener('submit', function (ev) {
-            if (!searchFormInput2 || searchFormInput2.value === '') {
-                alert('You must fill in the form');
-                ev.preventDefault();
-            }
-        });
-    }
-
-
+    //
+    // var searchForm2 = document.querySelector('.search-formSmall');
+    // var searchFormInput2 = document.querySelector('.searchInputSmall');
+    // if (searchForm2) {
+    //     searchForm2.addEventListener('submit', function (ev) {
+    //         if (!searchFormInput2 || searchFormInput2.value === '') {
+    //             alert('You must fill in the form');
+    //             ev.preventDefault();
+    //         }
+    //     });
+    // }
 
 })();
